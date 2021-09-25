a8effd6cdffbeefaf774d2973e25f53117a22c62
Victor H. Salcedo <victorsalc2@gmail.com>
2021-09-25 15:27:19 +0000

    finishing project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { TrafficLights } from "./TrafficLights";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
 		<>
-			<div className="text-center mt-5"></div>
+			<div className="trafficTop text-center mt-5">hello</div>
 			<div className="trafficLights">
 				<TrafficLights />
 			</div>
